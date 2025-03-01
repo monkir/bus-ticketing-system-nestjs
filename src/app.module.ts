82736b5366826da561cd6e01be4cc412effdceda
monkir <monkirchowdhury@gmail.com>
2025-03-01 20:06:16 +0600

    env file added

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,32 +7,34 @@ import { busownerModule } from './busowner/busowner.module';
 import { customerModule } from './customer/customer.module';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { othersModule } from './others/others.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     EmployeeModule,
     busownerModule,
     customerModule,
     othersModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DATABASE_HOST,
       port: 5432,
-      username: 'postgres',
-      password: 'tiger123',
-      database: 'bus-ticketing-system',
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABSE,
       autoLoadEntities: true,
       synchronize: true,
     }),
     MailerModule.forRoot({
       transport: {
-        host: 'smtp.gmail.com',
-        port: 465,
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
         ignoreTLS: true,
         secure: true,
         auth: {
-          user: 'bus.ticketing.system.atp@gmail.com',
-          pass: ''
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
         }
       }
     })
